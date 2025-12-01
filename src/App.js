bf2d9a81e55dc3438432ed6b33af8dba22e251e9
HEEJI YUN <93460560+Rodgershyeonsik@users.noreply.github.com>
2025-12-01 22:40:18 +0900

    refactor: replace hardcoded squares with nested loop rendering in Board

--- src/App.js
+++ src/App.js
@@ -18,25 +18,22 @@ function Board({ squares, xIsNext, onPlay }) {
 
   const winner = calculateWinner(squares);
   let status = winner ? "Winner: " + winner : "Next player: " + (xIsNext ? "X" : "O");
+  const board = [];
+
+  for(let i = 0; i < 3; i++) {
+    const boardRow = [];
+    
+    for(let j = 0; j < 3; j++) {
+      const idx = j + i * 3
+      boardRow.push(<Square key={idx} value={squares[idx]} onSquareClick={() => handleClick(idx)} />)
+    }
+    board.push(<div key={i} className='board-row'>{boardRow}</div>);
+  }
 
   return (
     <>
       <div className='status'>{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {board}
     </>
   );
 }
